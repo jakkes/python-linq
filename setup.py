f0bad41c2401bb3738dd7455fec681d90557b74d
Jakob Stigenberg <jakob@jakke.se>
2018-06-06 22:09:48 +0200

    First version available

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python-linq",
+    version="0.0.1",
+    author="Jakob Stigenberg",
+    description="Brings LINQ to Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jakkes/python-linq",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
