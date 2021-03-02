6af8d6cd97c363e29ede95300604d7607273fde9
Jakob Stigenberg <jakob@jakke.se>
2021-03-02 13:58:38 +0100

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="python-linq",
-    version="1.3.0",
+    version="1.4.0",
     author="Jakob Stigenberg",
     description="Brings LINQ to Python",
     long_description=long_description,
