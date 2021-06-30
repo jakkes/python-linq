92d909a10575a1362ef630110bca915093ad8903
Jakob Stigenberg <jakob@jakke.se>
2021-06-30 13:32:02 +0200

    add test

--- tests/test_distributed_query.py
+++ tests/test_distributed_query.py
@@ -2,18 +2,35 @@ from linq import DistributedQuery
 
 
 def square(x):
-    return x*x
+    return x * x
+
 
 def smaller_than_10(x):
     return x < 10
 
 
+def greater_than_0(x):
+    return x > 0
+
+
 def test_distributed_query_single_process():
-    assert DistributedQuery(range(100), processes=1).select(square).max() == 99**2
+    assert DistributedQuery(range(100), processes=1).select(square).max() == 99 ** 2
 
 
 def test_distributed_query_multiple_processes():
     assert DistributedQuery(range(100), processes=4).select(square).min() == 0
+
+
+def test_long_query():
+    assert (
+        DistributedQuery(range(100), processes=1)
+        .select(square)
+        .where(smaller_than_10)
+        .select(square)
+        .where(greater_than_0)
+        .count()
+        == 3
+    )
 
 
 def test_count():
@@ -21,17 +38,22 @@ def test_count():
 
 
 def test_where():
-    assert DistributedQuery(range(100), processes=1).where(smaller_than_10).count() == 10
+    assert (
+        DistributedQuery(range(100), processes=1).where(smaller_than_10).count() == 10
+    )
 
 
 def test_contains_1():
     assert DistributedQuery(range(100), processes=1).contains(50)
 
-def test_contains_2():    
+
+def test_contains_2():
     assert not DistributedQuery(range(100), processes=1).contains(-1)
 
-def test_contains_3():    
+
+def test_contains_3():
     assert 50 in DistributedQuery(range(100), processes=1)
 
-def test_contains_4():    
+
+def test_contains_4():
     assert -1 not in DistributedQuery(range(100), processes=1)
